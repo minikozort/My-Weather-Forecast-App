d2643452d8eead0518735b837f81604e7d7ebc93
minikozort <gokselgokkocabas@gmail.com>
2024-05-20 15:39:53 -0400

    edited css and script.js files to dynamically create the web page

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,37 @@
+
 let apiData = [];
 const mainUrl = "https://api.openweathermap.org";
 const cityEntered = document.getElementById("cityName");
 const appendSearch = document.getElementById("appendSearch");
 const searchButton = document.getElementById("searchButton");
-const savedSearch = [];
+const mainElement = document.querySelector('main');
+let savedSearch = []; 
+
+window.onload = function() {
+  const savedSearchJSON = localStorage.getItem("savedSearch");
+  if (savedSearchJSON) {
+    savedSearch = JSON.parse(savedSearchJSON);
+    savedSearch.forEach(cityName => {
+      displaySavedCity(cityName);
+    });
+  }
+};
+
+function displaySavedCity(cityName) {
+  const displayArea = document.createElement("div");
+  const cityButton = document.createElement("button");
+  cityButton.textContent = cityName;
+  cityButton.setAttribute('type', 'button');
+  cityButton.setAttribute('class', 'btn-info cityButton')
+  cityButton.setAttribute('id', cityName);
+  displayArea.append(cityButton);
+  appendSearch.append(displayArea);
+
+  cityButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    getCityApi(cityName); // Pass the city name to getCityApi function
+  });
+}
 
 function saveCityNameandDisplay() {
   const searchText = cityEntered.value.trim();
@@ -28,55 +56,35 @@ function saveCityNameandDisplay() {
   localStorage.setItem("savedSearch", JSON.stringify(savedSearch));
 
   // Display the saved search
-  const displayArea = document.createElement("div");
-  const cityButton = document.createElement("button");
-  cityButton.textContent = `${searchText}`;
-  cityButton.setAttribute('type' , 'button');
-  cityButton.setAttribute('id', searchText);
-  displayArea.append(cityButton);
-  appendSearch.append(displayArea);
+  displaySavedCity(searchText);
 
   cityEntered.value = "";
-
-
-  cityButton.addEventListener('click' , function (event){
-    event.preventDefault()
-    
-    getCityApi(searchText); // Pass the city name to getCityApi function
-  
-  });
 }
 
 // Using weather API to pull Lat and Lon of the city entered in the search box
 function getCityApi(cityName) {
-//   if (!cityName) {
-//     alert("No city name provided.");
-//     return;
-// }
+  let url = `${mainUrl}/data/2.5/forecast?cnt=1&q=${cityName}&appid=${apiKey}`;
 
-let url = `${mainUrl}/data/2.5/forecast?cnt=1&q=${cityName}&appid=${apiKey}`;
-
-fetch(url)
+  fetch(url)
     .then(function(response) {
-        return response.json();
+      return response.json();
     })
     .then(function(data) {
-        localStorage.setItem("forecast", JSON.stringify(data));
-        apiData.push(data);
-        const location = data.city.coord;
-        return location;
+      localStorage.setItem("forecast", JSON.stringify(data));
+      apiData.push(data);
+      const location = data.city.coord;
+      return location;
     })
     .then(function(location) {
-        getCityWeather(location);
-        get5DayForecast(location);
+      getCityWeather(location);
+      get5DayForecast(location);
     })
     .catch(function(error) {
-        console.error('Error fetching data:', error);
+      alert('Error fetching data');
     });
 }
 
 function getCityWeather(coords) {
-  //  Using Weather API to pull details of weather
   let urlLatLon = `${mainUrl}/data/2.5/forecast?cnt=1&units=imperial&lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`;
   fetch(urlLatLon)
     .then(function (response) {
@@ -84,13 +92,14 @@ function getCityWeather(coords) {
     })
     .then(function (data) {
       const temp = data.list[0].main.temp;
-      const icon = data.list[0].weather[0].icon;
+      const iconCode = data.list[0].weather[0].icon;
+      const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
       const humidity = data.list[0].main.humidity;
       const wind = data.list[0].wind.speed;
       const cityName = data.city.name;
+      const date = data.list[0].dt_txt; // Get the date
 
       const toRemove = document.getElementById("bodyDivClass");
-    
 
       if (toRemove != null) {
         toRemove.remove();
@@ -100,27 +109,37 @@ function getCityWeather(coords) {
       const secondDiv = document.createElement("div");
       const bodyDiv = document.createElement("div");
       bodyDiv.setAttribute("id", "bodyDivClass");
+      bodyDiv.setAttribute('class', "today card bg-light ");
+      const mainDivHeader = document.createElement('h2');
+      mainDivHeader.textContent = ("Current Weather");
+
       const cityHeader = document.createElement("h2");
-      const iconIcon = document.createElement("img");
+      const iconImage = document.createElement("img");
       const tempHeader = document.createElement("p");
       const humidHeader = document.createElement("p");
       const windHeader = document.createElement("p");
+      const dateHeader = document.createElement("h2"); // Create dateHeader element
+
+      // Format the date
+      const dateFormatted = dayjs(date).format("MM/DD/YYYY");
+      dateHeader.textContent = dateFormatted; // Set the formatted date
 
       cityHeader.textContent = `${cityName}`;
       tempHeader.textContent = `Temp: ${temp} °F`;
-      iconIcon.textContent = `${icon}`;
+      iconImage.src = iconUrl;
+      iconImage.alt = "Weather Icon";
       humidHeader.textContent = `Humidity: ${humidity} % `;
       windHeader.textContent = `Wind Speed: ${wind} mph`;
 
       mainDiv.append(secondDiv);
       secondDiv.append(bodyDiv);
-      bodyDiv.append(cityHeader, iconIcon, tempHeader, humidHeader, windHeader);
-      document.body.appendChild(bodyDiv);
+      bodyDiv.append(dateHeader, cityHeader, iconImage, tempHeader, humidHeader, windHeader); // Include dateHeader
+      mainElement.appendChild(bodyDiv);
     });
 }
 
+
 function get5DayForecast(coords) {
-  //  Using Weather API to pull details of weather
   let urlLatLon = `${mainUrl}/data/2.5/forecast?&units=imperial&lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`;
   fetch(urlLatLon)
     .then(function (response) {
@@ -128,34 +147,45 @@ function get5DayForecast(coords) {
     })
     .then(function (data) {
       cityData = data;
-      const forecastDiv = document.createElement("div");
-      const header = document.createElement("h2");
-      header.textContent = "5-Day Forecast";
-      forecastDiv.append(header);
 
+      
       const toRemove = document.getElementById("containerDiv");
-
 
       if (toRemove != null) {
         toRemove.remove();
       }
+
+      const header = document.createElement("h2");
+      header.setAttribute('id', 'sticky-top');
+      header.textContent = "5-Day Forecast";
+
       const container = document.createElement("div");
       container.setAttribute("id", "containerDiv");
-      container.append(forecastDiv);
+      container.setAttribute("class", "container-Div justify-space-between");
+
+      // Append header to the container
+      container.appendChild(header);
+
+      // Create a div to hold forecast cards
+      const forecastContainer = document.createElement("div");
+      forecastContainer.classList.add("forecast-container"); // Add class for styling
+      container.appendChild(forecastContainer);
+      
 
       for (var i = 1, len = data.list.length; i < len; i++) {
         const temp = data.list[i].main.temp;
-        const icon = data.list[i].weather[0].icon;
+        const iconCode = data.list[i].weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
         const humidity = data.list[i].main.humidity;
         const wind = data.list[i].wind.speed;
         const date = data.list[i].dt_txt;
 
         if (data.list[i].dt_txt.includes(`12:00:00`)) {
           const mainDiv = document.createElement("div");
-          const secondDiv = document.createElement("div");
-          const bodyDiv = document.createElement("div");
+          mainDiv.classList.add("forecast-card"); // Add class for styling
           const dateHeader = document.createElement("h2");
-          const iconIcon = document.createElement("img");
+          dateHeader.setAttribute('class', 'date-header');
+          const iconImage = document.createElement("img");
           const tempHeader = document.createElement("p");
           const humidHeader = document.createElement("p");
           const windHeader = document.createElement("p");
@@ -163,26 +193,23 @@ function get5DayForecast(coords) {
 
           dateHeader.textContent = dateFormatted;
           tempHeader.textContent = `Temp: ${temp} °F`;
-          iconIcon.textContent = `${icon}`;
+          iconImage.src = iconUrl;
+          iconImage.alt = "Weather Icon";
           humidHeader.textContent = `Humidity: ${humidity} % `;
           windHeader.textContent = `Wind Speed: ${wind} mph`;
 
-          mainDiv.append(secondDiv);
-          secondDiv.append(bodyDiv);
-          bodyDiv.append(
-            dateFormatted,
-            iconIcon,
-            tempHeader,
-            humidHeader,
-            windHeader
-          );
+          mainDiv.append(dateHeader, iconImage, tempHeader, humidHeader, windHeader);
 
-          container.append(bodyDiv);
-          document.body.appendChild(container);
+          // Append each forecast card to the forecast container
+          forecastContainer.appendChild(mainDiv);
         }
       }
+
+      // Append the container to the body
+      document.body.appendChild(container);
     });
 }
+
 
 searchButton.addEventListener("click", function (event) {
   event.preventDefault();
